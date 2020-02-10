a0cf695aa0b895b04f27d8ae9a01ae392b1e8883
Øyvind Skaar <os@odots.org>
2020-02-10 20:40:09 +0100

    Updated gcloud scripts. Awesome secrets handling

--- blogbackend/build-and-run-docker-image-locally.sh
+++ blogbackend/build-and-run-docker-image-locally.sh
@@ -5,6 +5,6 @@ IMAGE_URL=oyvindksk-blog:latest
 # don't use :latest when deploying, keep it ..
 sudo docker rmi ${IMAGE_URL}
 
-sudo docker build -t ${IMAGE_URL} .
+sudo docker build -t ${IMAGE_URL} -f ./SECRET-Dockerfile .
 
 sudo docker run -ti --rm -p 8080:8080 ${IMAGE_URL}
--- blogbackend/build-with-google-cloud-build.sh
+++ blogbackend/build-with-google-cloud-build.sh
@@ -1,3 +1,15 @@
-IMAGE_URL=gcr.io/stunning-symbol-139515/web-oyvindsk.com    # The docker (or..?) image to deploy, uses tagged :latest ? fixme
+IMAGE_URL=gcr.io/stunning-symbol-139515/web-oyvindsk.com    # The docker (or..?) image to deploy, uses tagged :latest ? FIXME
 
+# Move the secret Dockerfile to the standard filename for gcloud
+mv SECRET-Dockerfile Dockerfile
+
+# Upload the files from this diretory and build it remotly in Google Cloud Build, 
+# see https://console.cloud.google.com/cloud-build/builds 
+# or    gcloud builds list
+#       gcloud builds list --ongoing
+#
+# If it's successful, the image is pushed to Google Container Registry: https://console.cloud.google.com/gcr/images/
 gcloud builds submit --tag $IMAGE_URL
+
+# Move the Dockerfile back
+mv Dockerfile SECRET-Dockerfile
