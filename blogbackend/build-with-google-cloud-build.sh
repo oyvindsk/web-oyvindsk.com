af34b28a12dd372d17f11740eb1cfae2f41385bd
Øyvind Skaar <os@odots.org>
2020-02-11 11:30:48 +0100

    Small text and script fixes

--- blogbackend/build-with-google-cloud-build.sh
+++ blogbackend/build-with-google-cloud-build.sh
@@ -1,3 +1,4 @@
+PROJECT=stunning-symbol-139515                              # GCP project 
 IMAGE_URL=gcr.io/stunning-symbol-139515/web-oyvindsk.com    # The docker (or..?) image to deploy, uses tagged :latest ? FIXME
 
 # Move the secret Dockerfile to the standard filename for gcloud
@@ -9,7 +10,7 @@ mv SECRET-Dockerfile Dockerfile
 #       gcloud builds list --ongoing
 #
 # If it's successful, the image is pushed to Google Container Registry: https://console.cloud.google.com/gcr/images/
-gcloud builds submit --tag $IMAGE_URL
+gcloud builds submit --project $PROJECT --tag $IMAGE_URL
 
 # Move the Dockerfile back
 mv Dockerfile SECRET-Dockerfile
