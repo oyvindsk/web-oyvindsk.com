9096f8a2cd4dcfa3cad5a2d651b6c839f346892a
Øyvind Skaar <os@odots.org>
2020-05-05 14:42:03 +0200

    asciidoc + tachyons: metadata in blogposts, test1

--- asciidoc-tachyons/postprocess.go
+++ asciidoc-tachyons/postprocess.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -17,14 +18,14 @@ const header = `<!DOCTYPE html>
 		<article>
   			<header class="bg-gold sans-serif">
     			<div class="mw9 center pa4 pt5-ns ph7-l">
-      				<time class="f6 mb2 dib ttu tracked"><small>27 July, 2015</small></time>
+      				<time class="f6 mb2 dib ttu tracked"><small>...DATE...</small></time>
       				<h3 class="f2 f1-m f-headline-l measure-narrow lh-title mv0">
         				<span class="bg-black-90 lh-copy white pa1 tracked-tight">
-          					Too many tools and frameworks
+          					...TITLE...
         				</span>
       				</h3>
-      				<h4 class="f3 fw1 georgia i">The definitive guide to the javascript tooling landscape in 2015.</h4>
-      				<h5 class="f6 ttu tracked black-80">By Adam Morse</h5>
+      				<h4 class="f3 fw1 georgia i">...SUBTITLE...</h4>
+      				<h5 class="f6 ttu tracked black-80">By ..AUTHOR..</h5>
     			</div>
   			</header>
 
@@ -100,6 +101,114 @@ func (mt tachyons) getClasses(tt html.TokenType, t html.Token, orgClasses string
 
 func main() {
 
+	// <div class="openblock">
+	// <div class="content">
+	// <div class="paragraph">
+	// <p>|| Adam Morse || Too many tools and frameworks: subTTT || 2015 || /foo/bar || Subtitle: The definitive guide to the javascript tooling landscape in 2015 ||</p>
+	// </div>
+	// </div>
+	// </div>
+
+	file, err := os.Open("doctor.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	z := html.NewTokenizer(file)
+
+	var exclude bool
+	var excludeAfter = []string{}
+	var foundString bool
+
+	/*
+		States at the beginning of the big for loop:
+
+		exclude			excludeAfter		foundString 		MEANING
+		0 				0 					0					Looking for start div
+		0 				0 					1					Done excluding!
+		0 				1 					0					N/A : Turned on together, exclude turns off when !excludeAfter
+		0 				1 					1					N/A : Turned on together, exclude turns off when !excludeAfter
+
+		1 				0 					0					N/A : Turned on together, excludeAfter is not turned off when !foundString
+		1 				0 					1					Emptied excludeAfter but haven't turned exclude off yet. BUG??
+		1 				1 					0					Looking for foundString. Excluding and saving tags in excludeAfter
+		1 				1 					1					Looking for end div, removing tags from excludeAfter
+	*/
+	var excludedCnt int
+
+	for {
+		tt := z.Next()
+
+		if tt == html.ErrorToken {
+			fmt.Println(z.Err().Error()) // FIXME
+			break                        // return
+		}
+
+		t := z.Token()
+
+		// Look for metadata if we haven't found it yet
+		if !foundString && !exclude {
+			if tt == html.StartTagToken && t.Data == "div" {
+				for i := range t.Attr {
+					if t.Attr[i].Key == "class" {
+						if t.Attr[i].Val == "openblock" {
+							exclude = true
+							excludeAfter = append(excludeAfter, "div")
+							fmt.Println("EXCLUDE ON")
+							break // assume just 1 class
+						}
+					}
+				}
+			}
+		} else if exclude {
+
+			fmt.Printf("--   %q   %q\n", t.Data, t.Type.String())
+
+			if foundString {
+
+				fmt.Printf("%#v\n", excludeAfter)
+
+				if len(excludeAfter) == 0 {
+					fmt.Println("EXCLUDE OFF")
+					exclude = false
+				} else {
+					if tt == html.EndTagToken && t.Data == excludeAfter[len(excludeAfter)-1] {
+						excludeAfter = excludeAfter[:len(excludeAfter)-1]
+					}
+				}
+
+			}
+
+			if !foundString {
+
+				if tt == html.StartTagToken {
+					excludeAfter = append(excludeAfter, t.Data)
+				}
+
+				if t.Type.String() == "Text" && strings.HasPrefix(t.Data, "||") {
+					fmt.Println("FOUND")
+					foundString = true
+				}
+			}
+		}
+
+		if exclude {
+			excludedCnt++
+			if excludedCnt > 20 {
+				fmt.Println("Error: Excluded too much from blogpost wehn looking for metadata, giving up!")
+				return // FIXME
+			}
+		} else {
+			fmt.Print(t.String())
+		}
+		// FIXME
+	}
+
+}
+
+func postprocess() {
+
 	myTachyons := tachyons{}
 
 	file, err := os.Open("blogpost.html")
@@ -110,14 +219,14 @@ func main() {
 
 	z := html.NewTokenizer(file)
 
-	fmt.Print(header)
+	fmt.Print(header) // FIXME
 
 	for {
 		tt := z.Next()
 
 		if tt == html.ErrorToken {
-			fmt.Println(z.Err().Error())
-			break // return
+			fmt.Println(z.Err().Error()) // FIXME
+			break                        // return
 		}
 
 		t := z.Token()
@@ -126,16 +235,23 @@ func main() {
 
 		if tt == html.StartTagToken {
 
-			// 				Purge all classes for Attr,
-
-			// Find and cut class attr, if any. Assume order of attrs is irrelevant
-			var orgClasses string
+			// Find and save the original classes, if any.
+			var orgClasses string // class string
+			oci := -1             // index to remove later
 			for i := range t.Attr {
 				if t.Attr[i].Key == "class" {
 					orgClasses = t.Attr[i].Val
-					t.Attr[i] = t.Attr[0]
-					t.Attr = t.Attr[1:]
+					oci = i
+					break // assume just 1 class
 				}
+			}
+
+			// Remove the original class attr. Assume order of is irrelevant
+			if oci >= 0 {
+				t.Attr[oci] = t.Attr[0]
+				t.Attr = t.Attr[1:]
+				// t.Attr[oci] = t.Attr[len(t.Attr)-1] // or just t.Attr[0] = t.Attr[oci] , t.Attr = t.Attr[1:] ??
+				// t.Attr = t.Attr[:len(t.Attr)-1]
 			}
 
 			add, classes := myTachyons.getClasses(tt, t, orgClasses)
@@ -145,45 +261,8 @@ func main() {
 			}
 		}
 
-		// for i := range t.Attr {
-		// 	//fmt.Printf("%d\t%+v\t%#v\n%q\n", i, a, a, tt.String())
-		// 	// tml.Attribute{Namespace:"", Key:"class", Val:"sect2"}
-		// 	// html.Render(os.Stdout, )
-
-		// 	if t.Attr[i].Key == "class" {
-		// 		// t.Attr[i].Val = "FOO"
-		// 		t.Attr[i] = html.Attribute{}
-		// 	}
-		// }
-
-		fmt.Print(t.String())
-
-		// switch {
-		// case tt == html.ErrorToken:
-		// 	// End of the document, we're done
-		// 	return // return z.Err()
-		// case tt == html.StartTagToken:
-		// 	t := z.Token()
-
-		// 	// fmt.Printf("%q\n", t.String())
-
-		// 	isAnchor := t.Data == "div"
-		// 	if isAnchor {
-		// 		// fmt.Println("We found a link!")
-
-		// 		for range t.Attr {
-		// 			// fmt.Printf("%d\t%+v\t%#v\n%q\n", i, a, a, tt.String())
-		// 			// html.Render(os.Stdout, )
-
-		// 			t.Attr = []html.Attribute{}
-		// 		}
-		// 	}
-
-		// 	//fmt.Printf("%q\n", z.Raw())
-
-		// }
-		// fmt.Printf("%s", z.Raw())
+		fmt.Print(t.String()) // FIXME
 	}
-	fmt.Print(footer)
+	fmt.Print(footer) // FIXME
 
 }
