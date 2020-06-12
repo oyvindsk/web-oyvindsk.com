cb1662698d9d4fb6a3d9d4ab464f944ea7054f2f
Øyvind Skaar <os@odots.org>
2020-06-12 19:41:19 +0200

    misc deployment stuff

--- build-with-google-cloud-build.sh
+++ build-with-google-cloud-build.sh
@@ -10,6 +10,8 @@ mv SECRET-Dockerfile Dockerfile
 #       gcloud builds list --ongoing
 #
 # If it's successful, the image is pushed to Google Container Registry: https://console.cloud.google.com/gcr/images/
+#
+# Will ignore (leave out) files from .gitignore by default!
 gcloud builds submit --project $PROJECT --tag $IMAGE_URL
 
 # Move the Dockerfile back
--- deploy-from-google-container-registry.sh
+++ deploy-from-google-container-registry.sh
@@ -4,4 +4,4 @@ REGION=europe-west1                                         # GCP Region
 SERVICE_NAME=web-oyvindsk--dot--com                         # Service Name in Cloud Run
 IMAGE_URL=gcr.io/stunning-symbol-139515/web-oyvindsk.com    # The docker (or..?) image to deploy, uses tagged :latest ? FIXME
 
-gcloud beta run deploy $SERVICE_NAME --project $PROJECT --platform managed --region $REGION --allow-unauthenticated --image $IMAGE_URL --concurrency 1000
+gcloud run deploy $SERVICE_NAME --project $PROJECT --platform managed --region $REGION --allow-unauthenticated --image $IMAGE_URL --concurrency 1000
